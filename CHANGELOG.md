1650246f6974c19699102802a5243f70ab88490c
ReanGD <reangd@yandex.ru>
2020-09-21 03:27:32 +0300

    add webview panel

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+
+## 0.1.0
+- Added panel with devdocs.io
+
+## 0.0.1
+- Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devdocs-io" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devdocs-io.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from devdocs-io!');
+	let searchCmd = vscode.commands.registerCommand('devdocs-io.search', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'devdocs',
+			'devdocs',
+			vscode.ViewColumn.Two,
+			{
+				enableScripts: true,
+				retainContextWhenHidden: true
+			}
+		);
+		panel.webview.html = getWebviewContent("http://localhost:9292/cpp/memory/shared_ptr");
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(searchCmd);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(url: string) {
+	const html = `
+	<style>iframe { position: fixed; top: 0; left: 0; width: 100%; height: 100%; frameborder: 0; }</style>
+	<iframe src=${url} />`;
+
+	return html;
+}
