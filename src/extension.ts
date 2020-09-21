bb7e4c43fe8dc38ce4c35d3e21f7d211d79746b3
ReanGD <reangd@yandex.ru>
2020-09-22 01:53:32 +0300

    support close tab

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,22 @@
 import * as vscode from 'vscode';
 
 
+class Settings {
+	getURL() {
+		return vscode.workspace.getConfiguration("devdocs-io").get("URL", "https://devdocs.io/").toString();
+	}
+}
+
 class Panel {
 	id: number;
+	removed: boolean;
+	parent: PanelStorage;
 	panel: vscode.WebviewPanel;
 
-    constructor() {
+    constructor(parent: PanelStorage) {
 		this.id = 1;
+		this.removed = false;
+		this.parent = parent;
 		this.panel = vscode.window.createWebviewPanel(
 			'devdocs',
 			'devdocs',
@@ -16,6 +26,13 @@ class Panel {
 				retainContextWhenHidden: true
 			}
 		);
+
+		this.panel.onDidDispose(
+			() => {
+				this.removed = true;
+				this.parent.onDestroyPanel();
+			}, null
+		);
 	}
 
 	goto(url: string) {
@@ -24,7 +41,9 @@ class Panel {
 		<style>iframe { position: fixed; top: 0; left: 0; width: 100%; height: 100%; frameborder: 0; }</style>
 		<iframe id=${this.id} src=${url} />`;
 
-		this.panel.webview.html = html;
+		if (!this.removed) {
+			this.panel.webview.html = html;
+		}
 	}
 }
 
@@ -35,8 +54,16 @@ class PanelStorage {
 		this.panels = [];
 	}
 
+	onDestroyPanel() {
+		this.panels.forEach(function(item, index, object) {
+			if (item.removed) {
+			  object.splice(index, 1);
+			}
+		});
+	}
+
 	add(url: string) {
-		let panel = new Panel();
+		let panel = new Panel(this);
 		panel.goto(url);
 		this.panels.push(panel);
 	}
@@ -48,12 +75,6 @@ class PanelStorage {
 		} else {
 			this.panels[len - 1].goto(url);
 		}
-	}
-}
-
-class Settings {
-	getURL() {
-		return vscode.workspace.getConfiguration("devdocs-io").get("URL", "https://devdocs.io/").toString();
 	}
 }
 
