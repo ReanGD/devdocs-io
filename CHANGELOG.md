9cee5227b20dd019feee0a819a764fdf0228154a
ReanGD <reangd@yandex.ru>
2020-09-21 05:33:43 +0300

    add search in current and in new tab

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
 # Change Log
 
 ## 0.1.0
+
 - Added panel with devdocs.io
 - Added settings: "URL"
 
 ## 0.0.1
+
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	let searchCmd = vscode.commands.registerCommand('devdocs-io.search', () => {
-		const panel = vscode.window.createWebviewPanel(
+
+class Panel {
+	id: number;
+	panel: vscode.WebviewPanel;
+
+    constructor() {
+		this.id = 1;
+		this.panel = vscode.window.createWebviewPanel(
 			'devdocs',
 			'devdocs',
 			vscode.ViewColumn.Two,
@@ -11,23 +16,64 @@ export function activate(context: vscode.ExtensionContext) {
 				retainContextWhenHidden: true
 			}
 		);
-		const devdocsURL = vscode.workspace.getConfiguration("devdocs-io").get("URL", "https://devdocs.io/").toString();
-		panel.webview.html = getWebviewContent(devdocsURL);
+	}
+
+	goto(url: string) {
+		this.id++;
+		const html = `
+		<style>iframe { position: fixed; top: 0; left: 0; width: 100%; height: 100%; frameborder: 0; }</style>
+		<iframe id=${this.id} src=${url} />`;
+
+		this.panel.webview.html = html;
+	}
+}
+
+class PanelStorage {
+	panels: Panel[];
+
+	constructor() {
+		this.panels = [];
+	}
+
+	add(url: string) {
+		let panel = new Panel();
+		panel.goto(url);
+		this.panels.push(panel);
+	}
+
+	last(url: string) {
+		let len = this.panels.length;
+		if (len === 0) {
+			this.add(url);
+		} else {
+			this.panels[len - 1].goto(url);
+		}
+	}
+}
+
+class Settings {
+	getURL() {
+		return vscode.workspace.getConfiguration("devdocs-io").get("URL", "https://devdocs.io/").toString();
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let panels = new PanelStorage();
+	let settings = new Settings();
+
+	let searchCmd = vscode.commands.registerCommand('devdocs-io.search', () => {
+		panels.last(settings.getURL());
 	});
 
-	context.subscriptions.push(searchCmd);
+	let searchNewTabCmd = vscode.commands.registerCommand('devdocs-io.searchNewTab', () => {
+		panels.add(settings.getURL());
+	});
+
+	context.subscriptions.push(searchCmd, searchNewTabCmd);
 }
 
 export function deactivate() {}
 
 function showMessage(test: string) {
 	vscode.window.showInformationMessage(test);
-}
-
-function getWebviewContent(url: string) {
-	const html = `
-	<style>iframe { position: fixed; top: 0; left: 0; width: 100%; height: 100%; frameborder: 0; }</style>
-	<iframe src=${url} />`;
-
-	return html;
 }
