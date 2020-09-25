86c1939757de742f07cb1ee4bbca5d8b25324a29
ReanGD <reangd@yandex.ru>
2020-09-25 05:16:21 +0300

    add respose handler

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,9 @@
 import { URL } from 'url';
+import * as zlib from 'zlib';
+import * as stream from 'stream';
 import * as settings from './settings';
 import * as httpProxy from 'http-proxy';
+import * as replaceStream from 'replacestream';
 import { ServerResponse, IncomingMessage, IncomingHttpHeaders } from 'http';
 
 
@@ -52,31 +55,103 @@ class Cookie {
     }
 }
 
-function copyHeaders(proxyRes: IncomingMessage, req: IncomingMessage, res: ServerResponse) {
-    if (req.httpVersion === '1.0') {
-        delete proxyRes.headers['transfer-encoding'];
+function decompessWrap(proxyRes: IncomingMessage): stream.Readable {
+    switch (proxyRes.headers['content-encoding']) {
+        case 'br':
+            return proxyRes.pipe(zlib.createBrotliDecompress());
+        case 'gzip':
+            return proxyRes.pipe(zlib.createGunzip());
+        case 'deflate':
+            return proxyRes.pipe(zlib.createInflate());
+        default:
+            return proxyRes;
+      }
+}
+
+class Response {
+    private proxyRes: IncomingMessage;
+    private req: IncomingMessage;
+    private res: ServerResponse;
+
+    constructor(proxyRes: IncomingMessage, req: IncomingMessage, res: ServerResponse) {
+        this.proxyRes = proxyRes;
+        this.req = req;
+        this.res = res;
     }
 
-    if (req.httpVersion === '1.0') {
-        proxyRes.headers.connection = req.headers.connection || 'close';
-    } else if (req.httpVersion !== '2.0' && !proxyRes.headers.connection) {
-        proxyRes.headers.connection = req.headers.connection || 'keep-alive';
-    }
-
-    Object.keys(proxyRes.headers).forEach(function(key) {
-        var header = proxyRes.headers[key];
-        if (header !== undefined) {
-            res.setHeader(key.trim(), header);
+    private copyHeaders() {
+        if (this.req.httpVersion === '1.0') {
+            delete this.proxyRes.headers['transfer-encoding'];
+            this.proxyRes.headers.connection = this.req.headers.connection || 'close';
+        } else if (this.req.httpVersion !== '2.0' && !this.proxyRes.headers.connection) {
+            this.proxyRes.headers.connection = this.req.headers.connection || 'keep-alive';
         }
-    });
+
+        Object.keys(this.proxyRes.headers).forEach((key) => {
+            var header = this.proxyRes.headers[key];
+            if (header !== undefined) {
+                this.res.setHeader(key.trim(), header);
+            }
+        });
 
 
-    if (proxyRes.statusCode) {
-        res.statusCode = proxyRes.statusCode;
+        if (this.proxyRes.statusCode) {
+            this.res.statusCode = this.proxyRes.statusCode;
+        }
+
+        if(this.proxyRes.statusMessage) {
+            this.res.statusMessage = this.proxyRes.statusMessage;
+        }
     }
 
-    if(proxyRes.statusMessage) {
-        res.statusMessage = proxyRes.statusMessage;
+    private decompessStream(input: stream.Readable, contentEncoding: string | undefined): stream.Readable {
+        switch (contentEncoding) {
+            case 'br':
+                return input.pipe(zlib.createBrotliDecompress());
+            case 'gzip':
+                return input.pipe(zlib.createGunzip());
+            case 'deflate':
+                return input.pipe(zlib.createInflate());
+            default:
+                return input;
+          }
+    }
+
+    private processCommon(input: stream.Readable): stream.Readable {
+        return input;
+    }
+
+    private processJs(input: stream.Readable): stream.Readable {
+        return input;
+    }
+
+    private processHtml(input: stream.Readable): stream.Readable {
+        let cookie = new Cookie(this.req.headers);
+        let mobileModeExpectedValue = settings.isMobileMode() ? "1" : "0";
+        let mobileModeActualValue = cookie.getValue("override-mobile-detect");
+        if (mobileModeActualValue !== mobileModeExpectedValue) {
+            this.res.setHeader('Set-Cookie', `override-mobile-detect=${mobileModeExpectedValue}`);
+        }
+        return input;
+    }
+
+    process(): void {
+        this.copyHeaders();
+        let contentType = this.proxyRes.headers["content-type"];
+        let contentEncoding = this.proxyRes.headers['content-encoding'];
+        let outStream;
+        if (contentType === undefined) {
+            outStream = this.processCommon(this.proxyRes);
+        } else if (contentType.startsWith("application/javascript")) {
+            this.res.setHeader('content-encoding', 'identity');
+            outStream = this.processJs(this.decompessStream(this.proxyRes, contentEncoding));
+        } else if (contentType.startsWith("text/html")) {
+            outStream = this.processHtml(this.proxyRes);
+        } else {
+            outStream = this.processCommon(this.proxyRes);
+        }
+
+        outStream.pipe(this.res);
     }
 }
 
@@ -96,23 +171,8 @@ function start(address: URL, proxyPort: number): void {
 
     proxyServer = httpProxy.createProxyServer(options).listen(proxyPort);
     proxyServer.on("proxyRes", function (proxyRes: IncomingMessage, req: IncomingMessage, res: ServerResponse) {
-        let contentType = proxyRes.headers["content-type"];
-        if (contentType === undefined) {
-            copyHeaders(proxyRes, req, res);
-            proxyRes.pipe(res);
-            return;
-        }
-
-        if (contentType.startsWith("text/html")) {
-            copyHeaders(proxyRes, req, res);
-            let cookie = new Cookie(req.headers);
-            let mobileModeExpectedValue = settings.isMobileMode() ? "1" : "0";
-            let mobileModeActualValue = cookie.getValue("override-mobile-detect");
-            if (mobileModeActualValue !== mobileModeExpectedValue) {
-                res.setHeader('Set-Cookie', `override-mobile-detect=${mobileModeExpectedValue}`);
-            }
-            proxyRes.pipe(res);
-        }
+        let r = new Response(proxyRes, req, res);
+        r.process();
     });
 }
 
